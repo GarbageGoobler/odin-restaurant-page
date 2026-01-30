fc87b401542f17cab87829948ab796317995a383
GarbageGoobler <velocityraptor314@proton.me>
2026-01-30 16:51:57 +0100

    Edit webpack configuration for common, dev and prod files

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,34 +1,7 @@
-import path from "node:path";
-import HtmlWebpackPlugin from "html-webpack-plugin";
+import { merge } from 'webpack-merge';
+import common from './webpack.common.js';
 
-export default {
+export default merge(common, {
   mode: "production",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(import.meta.dirname, "dist"),
-    clean: true,
-  },
   devtool: "source-map",
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.html$/i,
-        use: ["html-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
-};
+});
